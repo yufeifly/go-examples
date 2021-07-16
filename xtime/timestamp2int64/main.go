b16aee6ae2460c5ce9af85b5be04a16f76430a17
“yufeifly” <yufei.xiong@qq.com>
2021-07-17 00:19:52 +0800

    time package 2

--- xtime/timestamp2int64/main.go
+++ xtime/timestamp2int64/main.go
@@ -17,7 +17,10 @@ func main() {
 	fmt.Println(tm.Format("02/01/2006 15:04:05 PM"))
 
 	//从字符串转为时间戳，第一个参数是格式，第二个是要转换的时间字符串
-	loc, _ := time.LoadLocation("Local")
+	loc, err := time.LoadLocation("Local")
+	if err != nil {
+		fmt.Println(err)
+	}
 	tm2, _ := time.ParseInLocation("2006-01-02 15:04", "2020-10-13 13:55", loc)
 	tm3 := tm2.Unix()
 	fmt.Println(tm3)
